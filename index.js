08e5ea9c17799618c15450ca7f738039c968a326
batman <batman_bt@yahoo>
2020-10-26 11:44:21 -0300

    Instalacao do modulo nodemon

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 // quando carregar o modulo faça associado a uma
 // constante para evitar a alteração do conteudo
 // e assim evitar erros de execucao .
+const express = require("express");
+
+const app = express();
+//  o modulo body-parser nos ajuda a capturar os
+// dados que virao no corpo de solicitacao e
+// realiza a sua conversao para json .
+// assim podemos manipular os dados
+const bodyParser = require("body-parser");
 
 // vamos iniciar os exemplos de utilizacao de
 // verbos http
@@ -17,13 +25,18 @@ app.get("/dados", (req, res) => {
 // utilizar quando o meu usuario envia algo ao
 // servidor com o intuito de cadastrar ou
 // realizar autenticacao
+app.use(bodyParser.json());
 app.post("/dados", (req, res) => {
-  res.send("voce esta no verbo POST");
+  res.send(req.body);
 });
 
 // PUT
+
+app.put("/dados", (req, res) => {
+  res.send("voce esta no verbo PUT");
+});
 // utlizando quando o usuario deseja apagar algum dado
-app.delete("/dados", (req.res) => {
+app.delete("/dados", (req, res) => {
   res.send("voce esta no verbo DELETE");
 });
 
