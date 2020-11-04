0de427a1632eae1e17526cc206ff48cd0b4e224a
batman <batman_bt@yahoo>
2020-11-04 09:34:42 -0300

    projeto de estudo NODE JS

--- index.js
+++ index.js
@@ -11,14 +11,42 @@ const app = express();
 // assim podemos manipular os dados
 const bodyParser = require("body-parser");
 
+// para ler o arquivo de json que contem os
+// produtos que desejo exibir Nós iremos
+// carregar o modulo do fs(file system).
+const fs = require("fs");
+
+// vamos criar uma variavel no fortmato de array
+// que ira guardar os produtos do arquivo loja
+var dadosprodutos = null;
+
+// realizar a leitura do arquivo de texto.
+// primeira parte é o nome do arquivo
+// segunda parte é o encoding(tipo texto-com acento)
+// terceira parte é a funcao de callback
+fs.readFile("./loja.json", "utf-8", function (err, texto) {
+  if (err) throw err;
+  dadosprodutos = JSON.parse(texto);
+});
+
 // vamos iniciar os exemplos de utilizacao de
 // verbos http
+
+var layout = [
+  {
+    header: "loja de produtos",
+    navegacao: "listar,cadastrar,atualizar,deletar",
+    main: "pagina do corpo",
+    footer: "Av. João Paulo, Vila Nova - São Paulo -SP",
+  },
+];
 
 // GET
 // quando o meu usuario deseja obter alguns dados
 // do servidor
-app.get("/dados", (req, res) => {
-  res.send("voce esta sendo no verbo GET");
+app.get("/listar", (req, res) => {
+  layout[0].main = dadosprodutos.produtos;
+  res.send(layout);
 });
 
 // POST
@@ -26,18 +54,57 @@ app.get("/dados", (req, res) => {
 // servidor com o intuito de cadastrar ou
 // realizar autenticacao
 app.use(bodyParser.json());
-app.post("/dados", (req, res) => {
-  res.send(req.body);
+app.post("/cadastrar", (req, res) => {
+  dadosprodutos.produtos.push(req.body);
+
+  fs.writeFile("./loja.json", JSON.stringify(dadosprodutos), "utf-8", function (
+    err
+  ) {
+    if (err) throw err;
+    res.send("dados cadastrados");
+  });
 });
 
 // PUT
+app.put("/atualizar", (req, res) => {
+  var idenviado = req.body.idproduto;
 
-app.put("/dados", (req, res) => {
-  res.send("voce esta no verbo PUT");
+  // pegar a quantidade de produtos deentro do arquivo json
+  var qtd = dadosprodutos.produtos.length;
+
+  for (var i = 0; i < qtd; i++) {
+    if (idenviado == dadosprodutos.produtos[i].idproduto) {
+      dadosprodutos.produtos[i].nome = req.body.nome;
+      dadosprodutos.produtos[i].descricao = req.body.descricao;
+      dadosprodutos.produtos[i].preco = req.body.preco;
+      dadosprodutos.produtos[i].imagem = req.body.imagem;
+      break;
+    }
+  }
+
+  fs.writeFile("./loja.json", JSON.stringify(dadosprodutos), "UTF-8", function (
+    err
+  ) {
+    if (err) throw err;
+    res.send("dados atualizadoscom sucesso!");
+  });
 });
 // utlizando quando o usuario deseja apagar algum dado
-app.delete("/dados", (req, res) => {
-  res.send("voce esta no verbo DELETE");
+app.delete("/apagar", (req, res) => {
+  var idenviado = req.body.idproduto;
+  var qtd = dadosprodutos.produtos.length;
+  for (var i = 0; i < qtd; i++) {
+    if (idenviado == dadosprodutos.produtos[i].idproduto) {
+      dadosprodutos.produtos.splice(i, 1);
+      break;
+    }
+  }
+  fs.writeFile("./loja.json", JSON.stringify(dadosprodutos), "utf-8", function (
+    err
+  ) {
+    if (err) throw err;
+    res.send("produto apagado.");
+  });
 });
 
 app.listen(3000);
